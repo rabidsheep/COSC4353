e07d3d70ff5c4aaad3f68f20c5e01d34d14cb315
Austin Jackson <themacphage@gmail.com>
2021-03-10 18:04:53 -0600

    remove and sort usings

--- OilTycoon/OilTycoon/Controllers/UserController.cs
+++ OilTycoon/OilTycoon/Controllers/UserController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OilTycoon.Database.ef;
 using OilTycoon.Database.interfaces;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
