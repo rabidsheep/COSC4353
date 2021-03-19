0c7a93ba39695157d6eee6ac661eac2595d39b40
Austin Jackson <themacphage@gmail.com>
2021-03-19 17:45:33 -0500

    technically doesn't have compiler errors

---
+++ OilTycoon/OilTycoon/ClientApp/src/routes/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+﻿import { Navigation, TitleArea } from '../../components/Reusables';
+
+// HTML for user homepage
+export function Contact() {
+	return (
+		<div id="logged-in">
+			<TitleArea />
+
+			<Navigation />
+			<div id="main">
+				<p>This is the contact page</p>
+			</div>
+		</div>
+	);
+}
---
+++ OilTycoon/OilTycoon/ClientApp/src/routes/Login/Login.tsx
@@ -0,0 +1,69 @@
+﻿import React from 'react';
+import { TitleArea } from '../../components/Reusables';
+
+export function Login(props: any) {
+    // Using this to test storing session data and switching pages
+    /*let user = {
+        username: '',
+        password: '',
+        loggedIn: false
+    }*/
+
+    /*const handleUsernameChange = (e: any) => {
+        user.username = e.target.value;
+    }
+
+    const handlePasswordChange = (e: any) => {
+        user.password = e.target.value;
+    }*/
+
+    const handleLoginSubmit = async (e: any) => {
+        e.preventDefault();
+
+        let username = e.target.elements.username.value;
+        let password = e.target.elements.password.value;
+
+        /* change this part later to instead verify login by connecting to backend */
+        let hardcodedCred = {
+            username: 'admin',
+            password: 'password'
+        }
+
+        if ((username === hardcodedCred.username) && (password === hardcodedCred.password)) {
+            console.log('you\'re logged in!');
+            localStorage.setItem('username', username);
+            localStorage.setItem('loggedIn', 'true');
+            console.log(e.target.elements.username.value);
+            props.history.push('/quote');
+        } else {
+            //bad combination
+            alert('wrong username or password combination');
+        }
+    }
+
+    // HTML for Login page (first page user should see)
+    return (
+        <div id="login">
+            <TitleArea />
+
+            <div id="login-wrapper">
+                <form id="login-form" onSubmit={handleLoginSubmit}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Username: </td>
+                                <td><input type="text" name="username" required /></td>
+                            </tr>
+                            <tr>
+                                <td>Password: </td>
+                                <td><input type="text" name="password" required /></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br />
+                    <button type="submit">Test login</button>
+                </form>
+            </div>
+        </div>
+    );
+}
