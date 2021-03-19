9adc34de645dedcf332c5419913c60b68ec671d6
Austin Jackson <themacphage@gmail.com>
2021-03-19 18:15:57 -0500

    Update UserHistory.tsx with example

--- OilTycoon/OilTycoon/ClientApp/src/routes/History/UserHistory.tsx
+++ OilTycoon/OilTycoon/ClientApp/src/routes/History/UserHistory.tsx
@@ -2,6 +2,45 @@ import { Navigation, TitleArea } from '../../components/Reusables';
 
 // HTML for user homepage
 export function UserHistory() {
+
+	let data: UserHistoryRowData[] = [
+		{
+			referenceNumber: '769560437',
+			dateRequested: '01-06-2021',
+			status: 'Processing',
+			terms: 'Express',
+			invoiceLink: '#'
+		},
+		{
+			referenceNumber: '754264326',
+			dateRequested: '12-28-2020',
+			status: 'Finalized',
+			terms: 'Standard',
+			invoiceLink: '#'
+		},
+		{
+			referenceNumber: '663256435',
+			dateRequested: '10-27-2020',
+			status: 'Finalized',
+			terms: 'Standard',
+			invoiceLink: '#'
+		},
+		{
+			referenceNumber: '601345772',
+			dateRequested: '09-04-2020',
+			status: 'Cancelled',
+			terms: 'Standard',
+			invoiceLink: '#'
+		},
+		{
+			referenceNumber: '455426347',
+			dateRequested: '03-24-2020',
+			status: 'Finalized',
+			terms: 'Standard',
+			invoiceLink: '#'
+		}
+	];
+
 	return (
 		<div id="logged-in">
 			<TitleArea />
@@ -22,44 +61,31 @@ export function UserHistory() {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>769560437</td>
-							<td>01-06-2021</td>
-							<td>Processing</td>
-							<td>Express</td>
-							<td> <a href="#"> <img src="static/pdf_png.png" alt={""} /> </a> </td>
-						</tr>
-						<tr>
-							<td>754264326</td>
-							<td>12-28-2020</td>
-							<td>Finalized</td>
-							<td>Standard</td>
-							<td> <a href="#"> <img src="static/pdf_png.png" alt={""} /> </a> </td>
-						</tr>
-						<tr>
-							<td>663256435</td>
-							<td>10-27-2020</td>
-							<td>Finalized</td>
-							<td>Standard</td>
-							<td> <a href="#"> <img src="static/pdf_png.png" alt={""} /> </a> </td>
-						</tr>
-						<tr>
-							<td>601345772</td>
-							<td>09-04-2020</td>
-							<td>Cancelled</td>
-							<td>Standard</td>
-							<td> <a href="#"> <img src="static/pdf_png.png" alt={""} /> </a> </td>
-						</tr>
-						<tr>
-							<td>455426347</td>
-							<td>03-24-2020</td>
-							<td>Finalized</td>
-							<td>Standard</td>
-							<td> <a href="#"> <img src="static/pdf_png.png" alt={""} /> </a> </td>
-						</tr>
+						{data.map(e => <UserHistoryRow key={e.referenceNumber} rowData={e} />)}
 					</tbody>
 				</table>
 			</div>
 		</div>
+	);
+}
+
+// temporary model to show sample code about how to do this
+interface UserHistoryRowData {
+	referenceNumber: string;
+	dateRequested: string;
+	status: string;
+	terms: string;
+	invoiceLink: string;
+}
+
+export function UserHistoryRow(props: { rowData: UserHistoryRowData }) {
+	return (
+		<tr>
+			<td>{props.rowData.referenceNumber}</td>
+			<td>{props.rowData.dateRequested}</td>
+			<td>{props.rowData.status}</td>
+			<td>{props.rowData.terms}</td>
+			<td> <a href={props.rowData.invoiceLink}> <img src="static/pdf_png.png" alt={""} /> </a> </td>
+		</tr>
 	);
 }
