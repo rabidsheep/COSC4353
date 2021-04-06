9d4b809d137cdc7e8a05309d5d6691bf908445ba
Austin Jackson <themacphage@gmail.com>
2021-04-05 21:51:06 -0500

    forgot this

--- OilTycoon/OilTycoon/ClientApp/src/generated/index.ts
+++ OilTycoon/OilTycoon/ClientApp/src/generated/index.ts
@@ -610,6 +610,50 @@ export class UserClient extends AuthorizedApiBase {
         }
         return Promise.resolve<User>(<any>null);
     }
+
+    /**
+     * @param username (optional) 
+     * @return Success
+     */
+    checkIfUsernameExists(username: string | undefined): Promise<boolean> {
+        let url_ = this.baseUrl + "/api/User/CheckIfUsernameExists?";
+        if (username === null)
+            throw new Error("The parameter 'username' cannot be null.");
+        else if (username !== undefined)
+            url_ += "username=" + encodeURIComponent("" + username) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "GET",
+            headers: {
+                "Accept": "text/plain"
+            }
+        };
+
+        return this.transformOptions(options_).then(transformedOptions_ => {
+            return this.http.fetch(url_, transformedOptions_);
+        }).then((_response: Response) => {
+            return this.transformResult(url_, _response, (_response: Response) => this.processCheckIfUsernameExists(_response));
+        });
+    }
+
+    protected processCheckIfUsernameExists(response: Response): Promise<boolean> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = resultData200 !== undefined ? resultData200 : <any>null;
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<boolean>(<any>null);
+    }
 }
 
 export class FuelQuote implements IFuelQuote {
